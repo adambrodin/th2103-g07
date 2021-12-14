043ff23a98510ed790f72c7c742a14e464493947
Adam Brodin <adambrodindeveloper@gmail.com>
2021-12-14 10:31:57 +0100

    feat(api): initialized typeorm in root

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  // TypeOrm settings comes from ormconfig.json & env variables
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [],
 })

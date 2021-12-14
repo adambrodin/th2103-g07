5ab39b8742ee4c9e15cc5f24d0d2d03b131e2204
Adam Brodin <adambrodindeveloper@gmail.com>
2021-12-14 14:10:41 +0100

    feat(api): ticket entity

--- api/src/entities/ticket.entity.ts
+++ api/src/entities/ticket.entity.ts
@@ -1,2 +1,35 @@
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { BookingEntity } from './booking.entity';
+import { TrainEntity } from './train.entity';
+
+export enum TicketType {
+  ADULT = 'Adult',
+  STUDENT = 'Student',
+  PENSIONER = 'Pensioner',
+}
+
+@Entity({ name: 'Ticket' })
+export class TicketEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  price: number;
+
   @Column({ type: 'enum', enum: TicketType, default: TicketType.ADULT })
   type: TicketType;
+
+  @ManyToMany(() => TrainEntity, (entity) => entity.tickets)
+  @JoinTable({ name: 'Ticket_Trains' })
+  train: TrainEntity;
+
+  @ManyToOne(() => BookingEntity, (entity) => entity.tickets)
+  booking: BookingEntity;
+}

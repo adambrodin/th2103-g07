eb1bde773d100a838558c5217abdc9059b0273bc
Adam Brodin <adambrodindeveloper@gmail.com>
2021-12-15 10:58:46 +0100

    feat(api): added global validation pipe

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api'); // GLOBAL ROUTE, CONTROLLERS ALWAYS START WITH /API/<CONTROLLER ROUTE>
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(1337);
 }
 bootstrap();

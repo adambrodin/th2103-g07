72d24a242b3ab6fcdde5508eab638d4acb1d6d22
Adam Brodin <adambrodindeveloper@gmail.com>
2021-12-15 12:02:58 +0100

    feat(api): basic search endpoint logic

--- api/src/controllers/booking.controller.ts
+++ api/src/controllers/booking.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { BookingService } from '../services/booking.service';
+import { TripSearchDto } from '@shared/dtos/trip-search.dto';
+import { Trip } from '../../../shared/models/trip';
 
 @Controller('booking')
 export class BookingController {
   constructor(private readonly _bookingService: BookingService) {}
+
+  @Post('search')
+  async searchAvailableTrips(
+    @Body() body: TripSearchDto,
+  ): Promise<{ response: string; data?: Trip[] }> {
+    const trips = await this._bookingService.getAvailableTrips(body);
+
+    if (trips.length > 0) {
+      return { response: 'Trips have been fetched successfully.', data: trips };
+    } else {
+      return { response: 'No trips were found for the given criteria.' };
+    }
+  }
 }
--- api/src/services/booking.service.ts
+++ api/src/services/booking.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Trip } from '@shared/models/trip';
+import { TripSearchDto } from '../../../shared/dtos/trip-search.dto';
 
 @Injectable()
-export class BookingService {}
+export class BookingService {
+  async getAvailableTrips(body: TripSearchDto): Promise<Trip[]> {
+    return [];
+  }
+}

ef535920391afeb2c2505eb10a24f3e310aee233
Adam Brodin <adambrodindeveloper@gmail.com>
2021-12-15 10:10:39 +0100

    feat(api): global controller 'api' prefix

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api'); // GLOBAL ROUTE, CONTROLLERS ALWAYS START WITH /API/<CONTROLLER ROUTE>
   await app.listen(1337);
 }
 bootstrap();

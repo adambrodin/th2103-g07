27a906670d6d2f81c5910f1a5272b2331ebc96dd
Kristianjimmefors <Kristian.jimmefors@gmail.com>
2021-12-15 12:07:31 +0100

    Added frontend for return trip selection

--- client/src/StartPage/startpage.tsx
+++ client/src/StartPage/startpage.tsx
@@ -1,10 +1,11 @@
-import { DatePicker } from 'rsuite';
-import React, { useState } from 'react';
-import SearchConponent from './ResultComponent';
+import { DatePicker } from "rsuite";
+import React, { useState } from "react";
+import SearchConponent from "./ResultComponent";
 
-import 'rsuite/dist/rsuite.min.css';
+import "rsuite/dist/rsuite.min.css";
 
 function StartPage() {
+  const [returnTrip, setReturnTrip] = useState(false);
   const [adultNum, setAdultNum] = useState(1);
   const [studentNum, setStudentNum] = useState(0);
   const [pensionerNum, setPensionerNum] = useState(0);
@@ -24,150 +25,151 @@ function StartPage() {
       kids: event.target[8].value,
     };
 
-    fetch('POst URL', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+    fetch("POst URL", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     }).then((res) => {
-      console.log('sucsess');
+      console.log("sucsess");
       ToggleSearchContainer();
     });
   }
 
   function toggleDatePicker() {
-    let x = document.getElementById('returnDate');
+    setReturnTrip(!returnTrip);
+    let x = document.getElementById("returnDate");
 
-    if (x!.style.display === 'inline') {
-      x!.style.display = 'none';
+    if (x!.style.display === "inline") {
+      x!.style.display = "none";
     } else {
-      x!.style.display = 'inline';
+      x!.style.display = "inline";
     }
   }
 
   function ToggleSearchContainer() {
-    let x = document.getElementById('SearchContainer');
-    let y = document.getElementById('searchResults');
-    let z = document.getElementById('backButton');
+    let x = document.getElementById("SearchContainer");
+    let y = document.getElementById("searchResults");
+    let z = document.getElementById("backButton");
     if (x != null && y != null && z != null) {
-      if (x.style.display === 'none') {
-        x.style.display = 'block';
-        y.style.display = 'none';
-        z.style.display = 'none';
+      if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
       } else {
-        x.style.display = 'none';
-        y.style.display = 'block';
-        z.style.display = 'block';
+        x.style.display = "none";
+        y.style.display = "block";
+        z.style.display = "block";
       }
     }
   }
 
   return (
-    <div className='container text-center'>
-      <div className='row'>
+    <div className="container text-center">
+      <div className="row">
         <h1>Tåg bokningssystem - Group 7</h1>
       </div>
-      <div id='SearchContainer' className='row mt-5'>
+      <div id="SearchContainer" className="row mt-5">
         <h2>Hej, Vart vill du resa?</h2>
-        <div className='justify-content-center'>
-          <form action='post' onSubmit={(event) => submitForm(event)}>
-            <div className='form-row'>
-              <div className='form-group col-md-8 mx-auto'>
-                <div id='startForm' className='input-group'>
-                  <label className='input-group-text' htmlFor='fromDestination'>
+        <div className="justify-content-center">
+          <form action="post" onSubmit={(event) => submitForm(event)}>
+            <div className="form-row">
+              <div className="form-group col-md-8 mx-auto">
+                <div id="startForm" className="input-group">
+                  <label className="input-group-text" htmlFor="fromDestination">
                     Från
                   </label>
                   <input
-                    id='fromDestination'
-                    className='form-control'
-                    type='text'
+                    id="fromDestination"
+                    className="form-control"
+                    type="text"
                   />
-                  <label className='input-group-text' htmlFor='toDestination'>
+                  <label className="input-group-text" htmlFor="toDestination">
                     Till
                   </label>
                   <input
-                    id='toDestination'
-                    className='form-control'
-                    type='text'
+                    id="toDestination"
+                    className="form-control"
+                    type="text"
                   />
                 </div>
               </div>
             </div>
-            <div className='form-row'>
-              <div className='form-group col-md-8 mx-auto'>
-                <div className='form-check form form-check-inline'>
-                  <label className='form-check-lable' htmlFor='returnTrip'>
+            <div className="form-row">
+              <div className="form-group col-md-8 mx-auto">
+                <div className="form-check form form-check-inline">
+                  <label className="form-check-lable" htmlFor="returnTrip">
                     Åter resa
                   </label>
                   <input
-                    className='form-check-input'
-                    type='checkbox'
-                    name='returnTrip'
-                    id='returnTrip'
+                    className="form-check-input"
+                    type="checkbox"
+                    name="returnTrip"
+                    id="returnTrip"
                     onChange={() => toggleDatePicker()}
                   />
                 </div>
-                <div id='timeSelectContainer'>
+                <div id="timeSelectContainer">
                   <>
                     <DatePicker
-                      format='yyyy-MM-dd HH:mm'
-                      placeholder='Avgångs tid'
+                      format="yyyy-MM-dd HH:mm"
+                      placeholder="Avgångs tid"
                     />
-                    <span id='returnDate'>
+                    <span id="returnDate">
                       <DatePicker
-                        format='yyyy-MM-dd HH:mm'
-                        placeholder='Ankomst tid'
+                        format="yyyy-MM-dd HH:mm"
+                        placeholder="Ankomst tid"
                       />
                     </span>
                   </>
                 </div>
                 <div>
-                  <label htmlFor=''>Vuxen</label>
+                  <label htmlFor="">Vuxen</label>
                   <input
-                    type='number'
-                    name=''
-                    id='adultTickets'
-                    min='0'
+                    type="number"
+                    name=""
+                    id="adultTickets"
+                    min="0"
                     value={adultNum}
                     onChange={(e: any) => setAdultNum(e.target.value)}
                   />
                 </div>
                 <div>
-                  <label htmlFor=''>Ungdom/Student</label>
+                  <label htmlFor="">Ungdom/Student</label>
                   <input
-                    type='number'
-                    name=''
-                    id='studentTickets'
-                    min='0'
+                    type="number"
+                    name=""
+                    id="studentTickets"
+                    min="0"
                     value={studentNum}
                     onChange={(e: any) => setStudentNum(e.target.value)}
                   />
                 </div>
                 <div>
-                  <label htmlFor=''>Pensionär</label>
+                  <label htmlFor="">Pensionär</label>
                   <input
-                    type='number'
-                    name=''
-                    id='pensionerTickets'
-                    min='0'
+                    type="number"
+                    name=""
+                    id="pensionerTickets"
+                    min="0"
                     value={pensionerNum}
                     onChange={(e: any) => setPensionerNum(e.target.value)}
                   />
                 </div>
                 <div>
-                  <label htmlFor=''>Barn</label>
+                  <label htmlFor="">Barn</label>
                   <input
-                    type='number'
-                    name=''
-                    id='kidsTicket'
-                    min='0'
+                    type="number"
+                    name=""
+                    id="kidsTicket"
+                    min="0"
                     value={kidsNum}
                     onChange={(e: any) => setKidsNum(e.target.value)}
                   />
                 </div>
                 <input
-                  className='btn btn-success mt-2'
-                  type='submit'
-                  value='Sök'
+                  className="btn btn-success mt-2"
+                  type="submit"
+                  value="Sök"
                 />
               </div>
             </div>
@@ -175,12 +177,13 @@ function StartPage() {
         </div>
       </div>
       <button
-        className='btn btn-secondary'
+        className="btn btn-secondary"
         onClick={() => ToggleSearchContainer()}
-        id='backButton'>
+        id="backButton"
+      >
         Tillbaka
       </button>
-      <SearchConponent />
+      <SearchConponent returnTrip={returnTrip} />
     </div>
   );
 }

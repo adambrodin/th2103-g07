0a66fd2012c51b18daa589278f8c85c77f812a9b
Adam Brodin <adambrodindeveloper@gmail.com>
2021-12-16 21:26:26 +0100

    feat(api): trip search error handling

--- api/src/controllers/booking.controller.ts
+++ api/src/controllers/booking.controller.ts
@@ -2,23 +2,30 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { BookingService } from '../services/booking.service';
 import { TripSearchDto } from '@shared/dtos/trip-search.dto';
 import { BookTripDto } from '@shared/dtos/book-trip.dto';
-import { Trip } from '@shared/models/trip';
 
 @Controller('booking')
 export class BookingController {
   constructor(private readonly _bookingService: BookingService) {}
 
   @Post('search')
-  async searchAvailableTrips(
-    @Body() body: TripSearchDto,
-  ): Promise<{ response: string; data?: Trip[] }> {
-    const trips = await this._bookingService.getAvailableTrips(body);
+  async searchAvailableTrips(@Body() body: TripSearchDto) {
+    const fetchedTrips = await this._bookingService.getAvailableTrips(body);
 
-    if (trips.length > 0) {
-      return { response: 'Trips have been fetched successfully.', data: trips };
+    if (fetchedTrips?.error != null) {
+      return {
+        response: 'An error occurred whilst fetching available trips.',
+        error: fetchedTrips?.error,
+      };
     }
 
-    return { response: 'No trips were found for the given criteria.' };
+    if (fetchedTrips.trips.length > 0) {
+      return {
+        response: 'Trips have been fetched successfully.',
+        data: fetchedTrips.trips,
+      };
+    } else {
+      return { response: 'No trips were found for the given criteria.' };
+    }
   }
 
   @Post('book')

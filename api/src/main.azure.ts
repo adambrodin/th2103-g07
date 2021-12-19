155cffab7e40c05466762edbb290277014b7b753
Adam Brodin <adambrodindeveloper@gmail.com>
2021-12-19 21:39:15 +0100

    feat(api): updated main.azure.ts

--- api/src/main.azure.ts
+++ api/src/main.azure.ts
@@ -1,11 +1,16 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { verifyTrainData } from './main';
 
 export async function createApp(): Promise<INestApplication> {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Import/populate the database if needed
+  await verifyTrainData();
+
   await app.init();
   return app;
 }

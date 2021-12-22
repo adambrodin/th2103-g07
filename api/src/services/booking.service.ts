355d3f72937a8154f2475a39fb53d8c37ef92324
Adam Brodin <adambrodindeveloper@gmail.com>
2021-12-22 16:00:45 +0100

    feat(api): bookTrip logic

--- api/src/services/booking.service.ts
+++ api/src/services/booking.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { BookTripDto } from '@shared/dtos/book-trip.dto';
-import { TripSearchDto } from '@shared/dtos/trip-search.dto';
 import { ReceiptEntity } from '../entities/receipt.entity';
 import { StationService } from './station.service';
 import { TripService } from './trip.service';
 import { Trip } from '@shared/models/trip';
 import { TripPoint } from '../../../shared/models/trip-point';
+import { getRepository } from 'typeorm';
+import { CustomerEntity } from '../entities/customer.entity';
+import { Customer } from '../../../shared/models/customer';
+import { TrainStopEntity } from '../entities/train-stop.entity';
+import { BookingEntity } from '../entities/booking.entity';
+import { TicketEntity } from '../entities/ticket.entity';
+import { TrainEntity } from '../entities/train.entity';
 
 @Injectable()
 export class BookingService {
@@ -48,7 +54,58 @@ export class BookingService {
     body: BookTripDto,
   ): Promise<{ error?: string; receipt?: ReceiptEntity }> {
     try {
+      const receiptRepo = getRepository(ReceiptEntity);
+      const ticketRepo = getRepository(TicketEntity);
+      const stopRepo = getRepository(TrainStopEntity);
+      const bookingRepo = getRepository(BookingEntity);
+      const customer = await this.fetchCustomer(body.customer);
+
+      const bookingStops: TrainStopEntity[] = [];
+      for (const stopId of body.trainStops) {
+        const bookingStop = await stopRepo.findOne({ where: { id: stopId } });
+        if (bookingStop == null) {
+          return { error: `TrainStop with id '${stopId}' could not be found.` };
+        }
+
+        bookingStops.push(bookingStop);
+      }
+
+      const train = await getRepository(TrainEntity)
+        .createQueryBuilder('train')
+        .leftJoinAndSelect('train.stops', 'stop')
+        .where('stop.id = :id', { id: bookingStops[0].id })
+        .getOne();
+
+      const booking = await bookingRepo.save({ customer: customer });
+      for (const seat of body.seats) {
+        await ticketRepo.save({
+          booking: booking,
+          stops: bookingStops,
+          train: train,
+          type: seat.ticket,
+        });
+      }
+
+      const savedReceipt = await receiptRepo.save({
+        booking: booking,
+        totalPrice: 100,
+      });
+
+      const receipt = await receiptRepo
+        .createQueryBuilder('receipt')
+        .where('receipt.id = :id', { id: savedReceipt.id })
+        .leftJoinAndSelect('receipt.booking', 'booking')
+        .leftJoinAndSelect('booking.tickets', 'ticket')
+        .leftJoinAndSelect('ticket.stops', 'stop')
+        .leftJoinAndSelect('stop.train', 'train')
+        .getOne();
+
+      return { receipt: receipt };
     } catch (err: any) {
+      return { error: err.message };
+    }
+  }
+
   async fetchCustomer(customerDetails: Customer): Promise<CustomerEntity> {
     const customerRepo = getRepository(CustomerEntity);
     let customer = await customerRepo.findOne({

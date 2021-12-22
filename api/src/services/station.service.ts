d2131a6a21f7df9ec203c1dfac895a2dfb839394
Adam Brodin <adambrodindeveloper@gmail.com>
2021-12-22 12:37:03 +0100

    feat(api): added getLocationSignature method

--- api/src/services/station.service.ts
+++ api/src/services/station.service.ts
@@ -1,10 +1,25 @@
-import { Injectable } from "@nestjs/common";
-import { TrainStopEntity } from "src/entities/train-stop.entity";
-import { getRepository } from "typeorm";
+import { Injectable } from '@nestjs/common';
+import { TrainStationEntity } from 'src/entities/train-station.entity';
+import { getRepository } from 'typeorm';
 
 @Injectable()
 export class StationService {
-    async getStationNames() {
-        return await getRepository(TrainStopEntity).createQueryBuilder('station').select('station.locationName').getMany();
+  async getStationNames() {
+    return await getRepository(TrainStationEntity)
+      .createQueryBuilder('station')
+      .select('station.locationName')
+      .getMany();
+  }
+
+  async getLocationSignature(locationName: string): Promise<string> {
+    const location = await getRepository(TrainStationEntity).findOne({
+      where: { locationName: locationName },
+    });
+
+    if (location != null) {
+      return location.locationSignature;
     }
+
+    return null;
+  }
 }

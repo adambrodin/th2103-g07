7532f62491abb1a8f04796f1fcb46922c6f493f6
Adam Brodin <adambrodindeveloper@gmail.com>
2021-12-29 13:29:50 +0100

    chore(api): misc formatting

--- api/src/controllers/station.controller.ts
+++ api/src/controllers/station.controller.ts
@@ -3,11 +3,10 @@ import { StationService } from 'src/services/station.service';
 
 @Controller('station')
 export class StationController {
+  constructor(private readonly _stationService: StationService) {}
 
-    constructor(private readonly _stationService: StationService) {}
-
-   @Get()
-   async getAllStations() {
-       return this._stationService.getStationNames();
-   } 
+  @Get()
+  async getAllStations() {
+    return this._stationService.getStationNames();
+  }
 }

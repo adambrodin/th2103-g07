59ec13cbd4b24721f00a3b8c687da689cece6bcd
Adam Brodin <adambrodindeveloper@gmail.com>
2021-12-29 13:23:06 +0100

    refactor(shared): updated trip response dto to include pricing estimates

--- shared/models/trip.ts
+++
@@ -1,9 +0,0 @@
-import { Train } from "./train";
-import { TripPoint } from "./trip-point";
-
-export class Trip {
-  train: Train;
-  departure: TripPoint;
-  arrival: TripPoint;
-  stops: TripPoint[];
-}

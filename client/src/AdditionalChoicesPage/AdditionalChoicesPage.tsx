b372b58b1c67188eb9b9da96256fd5428bc42172
Adam Brodin <adambrodindeveloper@gmail.com>
2022-01-13 15:40:03 +0100

    fix(client): fixed missing bracket

--- client/src/AdditionalChoicesPage/AdditionalChoicesPage.tsx
+++ client/src/AdditionalChoicesPage/AdditionalChoicesPage.tsx
@@ -1,18 +1,18 @@
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { BookingContext } from '../Contexts/BookingContext';
-import Stack from '@mui/material/Stack';
-import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
-import { useNavigate } from 'react-router-dom';
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { BookingContext } from "../Contexts/BookingContext";
+import Stack from "@mui/material/Stack";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
 
-import 'rsuite/dist/rsuite.min.css';
-import ChoachPickerComponent from './CoachPickerComponent';
+import "rsuite/dist/rsuite.min.css";
+import ChoachPickerComponent from "./CoachPickerComponent";
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.secondary,
 }));
 
@@ -41,19 +41,20 @@ const AdditionalChoicesPage = () => {
       price: Math.round(
         context.dbData.OutboundTrips[0].estimatedPrices[2].price
       ),
+    },
   ];
 
   function coachHandler(toggledCoachId: string) {
     const coach = options.find(({ id }) => id === toggledCoachId);
     if (coach) {
-      if (coach.name === 'Tyst vagn') {
+      if (coach.name === "Tyst vagn") {
         if (context.searchData.returnTrip === false) {
           updateContext({
             ...context,
             SelectedTrain: {
               ...context.SelectedTrain,
               TotalTicketPrice: coach.price,
-              class: 'Quiet Cart',
+              class: "Quiet Cart",
             },
           });
         } else {
@@ -62,23 +63,23 @@ const AdditionalChoicesPage = () => {
             SelectedTrain: {
               ...context.SelectedTrain,
               TotalTicketPrice: coach.price,
-              class: 'Quiet Cart',
+              class: "Quiet Cart",
             },
             SelectedReturnTrain: {
               ...context.SelectedReturnTrain,
               TotalTicketPrice: coach.price,
-              class: 'Quiet Cart',
+              class: "Quiet Cart",
             },
           });
         }
-      } else if (coach.name === 'Djurvagn') {
+      } else if (coach.name === "Djurvagn") {
         if (context.searchData.returnTrip === false) {
           updateContext({
             ...context,
             SelectedTrain: {
               ...context.SelectedTrain,
               TotalTicketPrice: coach.price,
-              class: 'Animal Friendly',
+              class: "Animal Friendly",
             },
           });
         } else {
@@ -87,23 +88,23 @@ const AdditionalChoicesPage = () => {
             SelectedTrain: {
               ...context.SelectedTrain,
               TotalTicketPrice: coach.price,
-              class: 'Animal Friendly',
+              class: "Animal Friendly",
             },
             SelectedReturnTrain: {
               ...context.SelectedReturnTrain,
               TotalTicketPrice: coach.price,
-              class: 'Animal Friendly',
+              class: "Animal Friendly",
             },
           });
         }
-      } else if (coach.name === 'Vanlig vagn') {
+      } else if (coach.name === "Vanlig vagn") {
         if (context.searchData.returnTrip === false) {
           updateContext({
             ...context,
             SelectedTrain: {
               ...context.SelectedTrain,
               TotalTicketPrice: coach.price,
-              class: 'Second Class',
+              class: "Second Class",
             },
           });
         } else {
@@ -112,12 +113,12 @@ const AdditionalChoicesPage = () => {
             SelectedTrain: {
               ...context.SelectedTrain,
               TotalTicketPrice: coach.price,
-              class: 'Second Class',
+              class: "Second Class",
             },
             SelectedReturnTrain: {
               ...context.SelectedReturnTrain,
               TotalTicketPrice: coach.price,
-              class: 'Second Class',
+              class: "Second Class",
             },
           });
         }
@@ -126,36 +127,37 @@ const AdditionalChoicesPage = () => {
   }
 
   function nextPage() {
-    nav('/payment');
+    nav("/payment");
   }
 
   return (
     <div>
-      <div className='container mt-5 text-center'>
-        <h1 className='font-color'>Tillval</h1>
+      <div className="container mt-5 text-center">
+        <h1 className="font-color">Tillval</h1>
       </div>
 
       <div className="container" id="additional-choices-list">
         <Stack spacing={2}>
-          <Item className='mb-4'>
-            <h4 className='mb-3'>Välj typ av vagn:</h4>
+          <Item className="mb-4">
+            <h4 className="mb-3">Välj typ av vagn:</h4>
             <ChoachPickerComponent options={options} handler={coachHandler} />
           </Item>
         </Stack>
       </div>
 
-      <div className='container'>
+      <div className="container">
         <div>
-          <Link to='/payment'>
+          <Link to="/payment">
             <button
-              id='continue-to-payment-btn'
-              className='btn confirm-button float-right'
-              onClick={nextPage}>
+              id="continue-to-payment-btn"
+              className="btn confirm-button float-right"
+              onClick={nextPage}
+            >
               Fortsätt
             </button>
           </Link>
-          <Link to='/results'>
-            <button id='back-to-results-btn' className='btn back-button'>
+          <Link to="/results">
+            <button id="back-to-results-btn" className="btn back-button">
               Tillbaka
             </button>
           </Link>
